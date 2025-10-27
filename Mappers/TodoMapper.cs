6f245f3311ee834228d4de2072167f3283ca6d8f
thiagoocms <thiagocms13@gmail.com>
2025-10-26 21:12:28 -0300

    adicionando o mapper

---
+++ Mappers/TodoMapper.cs
@@ -0,0 +1,20 @@
+﻿using demo.Dto;
+using demo.Models;
+
+namespace demo.Mappers;
+
+public class TodoMapper
+{
+    public static TodoDto ToDto(Todo todo) => new()
+    {
+        Id = todo.Id,
+        Title = todo.Title,
+        Checked = todo.Checked
+    };
+
+    public static Todo ToEntity(TodoDto dto) => new(dto.Title)
+    {
+        Id = dto.Id,
+        Checked = dto.Checked
+    };
+}
--- Services/TodoService.cs
+++ Services/TodoService.cs
@@ -1,4 +1,5 @@
 ﻿using demo.Dto;
+using demo.Mappers;
 using demo.Models;
 using demo.Repositories;
 
@@ -8,16 +9,11 @@ public class TodoService(TodoRepository repository)
 {
     public async Task<TodoDto> CreateAsync(TodoDto dto)
     {
-        var todo = new Todo(dto.Title);
+        var todo = TodoMapper.ToEntity(dto);
 
         await repository.SaveAsync(todo);
 
-        return new TodoDto
-        {
-            Id = todo.Id,
-            Title = todo.Title,
-            Checked = todo.Checked
-        };
+        return TodoMapper.ToDto(todo);
     }
 
     public async Task<TodoDto> UpdateAsync(int id, TodoDto dto)
@@ -29,12 +25,7 @@ public class TodoService(TodoRepository repository)
 
         await repository.SaveAsync(todo);
 
-        return new TodoDto
-        {
-            Id = todo.Id,
-            Title = todo.Title,
-            Checked = todo.Checked
-        };
+        return TodoMapper.ToDto(todo);
     }
 
     public async Task DeleteByIdAsync(int id)
@@ -47,25 +38,15 @@ public class TodoService(TodoRepository repository)
     public async Task<List<TodoDto>> GetAllAsync()
     {
         var todos = await repository.GetAllAsync();
-        
-        return todos.ConvertAll(item => new TodoDto
-        {
-            Id = item.Id,
-            Title = item.Title,
-            Checked = item.Checked
-        });
+
+        return todos.Select(TodoMapper.ToDto).ToList();
     }
 
     public async Task<TodoDto> GetByIdAsync(int id)
     {
         var todo = await GetCheckByIdAsync(id);
 
-        return new TodoDto
-        {
-            Id = todo.Id,
-            Title = todo.Title,
-            Checked = todo.Checked
-        };
+        return TodoMapper.ToDto(todo);
     }
 
     private async Task<Todo> GetCheckByIdAsync(int id)
